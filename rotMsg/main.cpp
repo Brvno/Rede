b40bfa4383bedab39945f1daa11521582e6c2820
Brvno <brvnoff@gmail.com>
2014-10-22 11:57:21 -0200

    .1

--- rotMsg/main.cpp
+++ rotMsg/main.cpp
@@ -11,36 +11,42 @@ using namespace std;
 ///Variaveis Globais
 tabela table;
 int mainSocket;
-vector<int> fdSocket;
-int socketOpen[10];
+vector<int> clientSocket; // Onde este ponto esta conectado
+int serverSocket[10];     // Onde me conecatam
 socklen_t sockLen;
 char messageTh[50];
 
-//Thread para responder Tunnel
-void *respTunnel(void *socket){
-
-    receiveMessage((int)socket, messageTh, sizeof(messageTh));
-    if(strcmp(messageTh, "Ping") ){
-        messageTh[0] = rand() % 150 + 1;
-        sendMessage((int)socket, messageTh, sizeof(messageTh));
+//Thread para receber mensagens
+void *serverResp(void*){
+    while(true){
+        for(int i = 0; i < 10; i++){
+            receiveMessage(serverSocket[i], messageTh, sizeof(messageTh));
+            /// Mensagem de ping
+            if(strcmp(messageTh, "Ping") ){
+                messageTh[0] = rand() % 150 + 1;
+                sendMessage(serverSocket[i], messageTh, sizeof(messageTh));
+            }
+            /// Mensagem de tabela
+            if(messageTh[0] == '#'){
+                //TODO: Atualizar tabela
+            }
+        }
     }
     pthread_exit(NULL);
 }
 
-
 //Conectar este ponto aos outros
 void iniciarCon(){
     for(int i = 0; i < 10; i++){
-        mainSocket = openConnection(PORT+i,0);
-        socketOpen[i] = acceptConnection(mainSocket, sockLen);
+        mainSocket = openConnection(PORT+i,1);
+        serverSocket[i] = acceptConnection(mainSocket, sockLen);
     }
     cout << "Quantas conexoes nesse ponto? ";
     int numCon;
     cin >> numCon;
 
-    pthread_t th[numCon];
 
-    fdSocket.resize(numCon);
+    clientSocket.resize(numCon);
     vector<ponto> aux;
     for(int i = 0; i < numCon; i++){
         ponto temp;
@@ -48,11 +54,12 @@ void iniciarCon(){
         cin >> temp.ip;
         aux.push_back(temp);
 
-        fdSocket[i] = tryConnection(temp.ip, PORT, 0);
-        pthread_create(&th, NULL, &respTunnel, NULL, (void*)fdSocket[i]);
+        clientSocket[i] = tryConnection(temp.ip, PORT, 0);
     }
     table.id = 0;
     table.grafo.push_back(aux);
+    pthread_t th;
+    pthread_create(&th, NULL, &serverResp, NULL);
 
     cout << "Conexao estabelecida" << endl;
 }
@@ -62,22 +69,37 @@ void tunnel(){
     cout << "--- Verificando Latencia das conexoes..." << endl;
     unsigned int p = table.grafo.size()-1;
     char message[10];
-    for(int i = 0; i < table.grafo[p]; i++){
+    for(unsigned int i = 0; i < table.grafo[p].size(); i++){
         cout << "Disparando pacotes em " << table.grafo[p][i].ip << endl;
         sleep(1);
-        message = "Ping";
-        sendMessage(fdSocket[i], message, sizeof(message));
-        receiveMessage(fdSocket[i], message, sizeof(message));
+        strcpy(message ,"Ping");
+        sendMessage(clientSocket[i], message, sizeof(message));
+        receiveMessage(clientSocket[i], message, sizeof(message));
         cout << "  time: " << (int) message[0] << " ms" << endl;
         table.grafo[p][i].ping = (int)message[0];
     }
     cout << "--- Done ---" << endl;
 }
 
-int main()
-{
-    iniciarCon();
-    tunnel();
-    pthread_join(th, NULL);
+//Enviar Tabela para os adj
+void enviarTabela(){
+/// FIXME: Como saber qual eh o indice deste pc? Ultimo(?)
+/// RESP: Quando for concatenar na nova tabela, manter a ultima linha.
+    char *msg = tabelaToString(table);
+    vector<ponto> aux = table.grafo.back();
+
+    for(unsigned int i = 0; i < aux.size(); i++){
+        cout << "Enviou tabela para: " << aux[i].ip << endl;
+        sendMessage(clientSocket[i], msg, sizeof(msg));
+        sleep(1);
+    }
 }
 
+int main(){
+    iniciarCon();
+    tunnel();
+    //pthread_join(th, NULL);
+    enviarTabela();
+    //djikstra();
+    //enviarMsg(dst);
+}
--- rotMsg/yggdrasil.h
+++ rotMsg/yggdrasil.h
@@ -14,5 +14,21 @@ struct tabela{
    int id;
 };
 
+char* tabelaToString(tabela t){
+    char final[500];
+    final[0] = (char)t.id;
+    final[1] = '#';
+    for(unsigned int i = 0; i < t.grafo.size(); i++){
+        for(unsigned int j = 0; j < t.grafo[i].size(); j++){
+            int pos = 2 + j*(i+1);
+            final[pos++] = t.grafo[i][j].ping;
+            final[pos] = '-';
+            strcat(final, t.grafo[i][j].ip);
+        }
+        final[t.grafo[i].size()] = ';';
+    }
+
+    return final;
+}
 
 #endif // STRUCTS_H
