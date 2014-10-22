0a5b486fa78e370bea6718767856995128de9bb1
Aluno <aluno@21xy-zz.(none)>
2014-10-22 16:43:37 -0200

    toda diferenca

--- rotMsg/main.cpp
+++ rotMsg/main.cpp
@@ -22,16 +22,13 @@ void *serverResp(void* s){
 
     receiveMessage(socket, messageTh, sizeof(messageTh));
     /// Mensagem de ping
-    if(strcmp(messageTh, "Ping") ){
-        messageTh[0] = rand() % 150 + 1;
-        sendMessage(socket, messageTh, sizeof(messageTh));
-    }
+    messageTh[0] = rand() % 150 + 1;
+    sendMessage(socket, messageTh, sizeof(messageTh));
+
     tabela t;
     recv(socket, (char*)&t, sizeof(tabela), 0);
     /// Mensagem de tabela
-    if(messageTh[1] == '#'){
-        //TODO: Atualizar tabela
-    }
+    cout << " --- ID" << t.id << endl;
 
     pthread_exit(NULL);
 }
@@ -51,13 +48,13 @@ void iniciarCon(){
     clientSocket.resize(numCon);
     vector<ponto> aux;
     for(int i = 0; i < numCon; i++){
+        pthread_create(&th[i], NULL, &serverResp, (void*)clientSocket[i]);
         ponto temp;
         cout << "IP alvo: ";
         cin >> temp.ip;
         aux.push_back(temp);
 
         clientSocket[i] = tryConnection(temp.ip, PORT, 0);
-        pthread_create(&th[i], NULL, &serverResp, (void*)clientSocket[i]);
     }
     table.id = 0;
     table.grafo.push_back(aux);
@@ -74,10 +71,11 @@ void tunnel(){
         cout << "Disparando pacotes em " << table.grafo[p][i].ip << endl;
         sleep(1);
         strcpy(message ,"Ping");
+        char ping[5];
         sendMessage(clientSocket[i], message, sizeof(message));
-        receiveMessage(clientSocket[i], message, sizeof(message));
-        cout << "  time: " << (int) message[0] << " ms" << endl;
-        table.grafo[p][i].ping = (int)message[0];
+        receiveMessage(clientSocket[i], ping, sizeof(ping));
+        cout << "  time: " << (int) ping << " ms" << endl;
+        table.grafo[p][i].ping = (int)ping[0];
     }
     cout << "--- Done ---" << endl;
 }
