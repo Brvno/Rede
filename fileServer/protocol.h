01476e01fb6ec0d51287b2fd7d7013979a1dae86
Brvno <brvnoff@gmail.com>
2014-12-13 13:40:29 -0200

    ...

--- fileServer/protocol.h
+++ fileServer/protocol.h
@@ -1,10 +1,22 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
 class Protocol
 {
+private:
+    int tipo;       // Podendo ser de 3. Post, Ask e Send
+    string nome;    // Nome do arquivo
+    int size;       // Tamanho do Arquivo
+    char* arquivo;  // Dados do arquivo
+    int checkSum;   // verificacao
 public:
     Protocol();
+    char* byterizar();
+    int checkSum(char*);
 };
 
 #endif // PROTOCOL_H
