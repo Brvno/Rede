c5cdce56c2dc827162c4c32edb1945c77fc70d21
Aluno <aluno@21xy-zz.(none)>
2014-12-17 16:50:44 -0200

    deu tnbm

--- trocaChaves/seguranca.cpp
+++ trocaChaves/seguranca.cpp
@@ -1,5 +1,5 @@
 #include "communication.h"
-#include<iostream>
+#include <iostream>
 
 #define PORT  50000
 #define PORT2 50001
@@ -9,7 +9,7 @@ char chave_ses,msg[100],minhachave,message[100];
 void criptografa()
 {
     int i=0;
-    while(msg[i]!='\0')
+    while(message[i]!='\0')
     {
         message[i]=message[i]+chave_ses;
         i++;
@@ -18,7 +18,7 @@ void criptografa()
 void descriptografa()
 {
     int i=0;
-    while(msg[i]!='\0')
+    while(message[i]!='\0')
     {
         message[i]=message[i]-chave_ses;
         i++;
@@ -38,9 +38,9 @@ int main(int argc, char *argv[])
 {
 	//inicializacao
 	banco[0].nome='A';
-	banco[0].chave='1';
+	banco[0].chave=60;
 	banco[1].nome='B';
-	banco[1].chave='2';
+	banco[1].chave=50;
 	char minhachavei,pc1,pc2; 
     /// Iniciar conexao
     int server;
@@ -50,8 +50,16 @@ int main(int argc, char *argv[])
     cout << "Servidor? [0/1/2]" <<endl;
     cin >> server;
     if(server==0 || server==2){
-        mainSocket = openConnection(PORT, 0);
-        fdSocket = acceptConnection(mainSocket, sockLen);
+        if(server==0)
+        {
+            mainSocket = openConnection(PORT, 0);
+            fdSocket = acceptConnection(mainSocket, sockLen);
+        }
+        else
+        {
+            mainSocket = openConnection(PORT2, 0);
+            fdSocket = acceptConnection(mainSocket, sockLen);
+        }            
 
     }
     else
@@ -65,14 +73,13 @@ int main(int argc, char *argv[])
     if(server==0)
     {
         char* msg;
-		 msg = (char*)malloc(3*sizeof(char));		
+		msg = (char*)malloc(3*sizeof(char));		
         receiveMessage(fdSocket,msg,sizeof(char)*100);
-        cout<<"mensagem eh ="<<msg<<endl;
-        cout<<"recebeu a primeira"<<endl;
+        cout<<"mensagem ==["<<msg[0]<<","<<msg[1]<<"]"<<endl;
         pc1=msg[0];
         pc2=msg[1];
         int chave1,chave2;
-        int chave3=3;
+        int chave3=30;
         for(int i=0;i<5;i++) 
         {
             if(pc1 == banco[i].nome)
@@ -80,72 +87,66 @@ int main(int argc, char *argv[])
             if(pc2 == banco[i].nome)
                 chave2=banco[i].chave;
         }
-        //chaveA,crip(A,B),chaveB,crip(a,b)
         char chave_encp31=(char)(chave3+chave1);
         char chave_encp32=(char)(chave3+chave2);
-        cout<<chave_encp31<<"  "<<chave_encp32;
         msg[0]=chave_encp31;
         msg[1]=chave_encp32;
         sendMessage(fdSocket,msg,sizeof(char)*100);
-        cout<<"mensagem eh"<<msg<<endl;
+        cout<<"mensagem ==["<<msg[0]<<","<<msg[1]<<"]"<<endl;
         closeConnection(fdSocket);
     }
     //se for pc 1
     if(server==1)
     {	
-    	minhachave=1;
+    	minhachave=60;
         char* msg;
-        msg = (char*)malloc(3*sizeof(char));		
+        msg = (char*)malloc(6*sizeof(char));		
         msg[0]='A';
         msg[1]='B';
-        msg[2] = '\0';
         //envia mensagem pro servidor
-         cout<<"mensagem eh ="<<msg<<endl;
+        cout<<"mensagem ==["<<msg[0]<<","<<msg[1]<<"]"<<endl;
         sendMessage(fdSocket, msg, sizeof(char)*100);
-        cout<<"enviou primeira"<<endl;
         //recebe mensagem do servidor
         receiveMessage(fdSocket,msg,sizeof(char)*100);
-         cout<<"mensagem eh ="<<msg<<endl;
-        cout<<"recebeu a criptografia"<<endl;
+        cout<<"mensagem ==["<<msg[0]<<","<<msg[1]<<"]"<<endl;
         chave_ses=(int)msg[0];
         chave_ses=chave_ses-minhachave;
-        cout<<"chave_ses="<<chave_ses<<endl;
         msg[0]='A';
         sendMessage(fdSocket2,msg,sizeof(char)*100);
 		//comeca mensagens
 		while(strcmp(message, "tchau")){
 			receiveMessage(fdSocket2,message,100*sizeof(char)); //Recebe mensagem
-			printf("mensagem criptografada dele: %s\n", message);
 			descriptografa();
-			printf("mensagem descriptografada dele: %s\n", message);
+			printf("Mensagem Recebida: %s\n\n", message);
 			if(!strcmp(message, "tchau"))
 				break;
+			cout<<"digite a mensagem"<<endl;	
 			gets(message);
-			printf("mensagem descriptografada minha: %s\n", message);
 			criptografa();
-			printf("mensagem criptografada minha: %s\n", message);
 			sendMessage(fdSocket2,message,100*sizeof(char)); //Envia mensagem
 
 		}
 
     }
 
-    //se for pc 2
+    //se for pc 'B'
     if(server==2)
     {
-    	minhachave=2;
+    	minhachave=50;
+    	//Recebe a mensagem do pc A
         receiveMessage(fdSocket,msg,100*sizeof(char));
+        cout<<"mensagem ==["<<msg[0]<<","<<msg[1]<<"]"<<endl;
         chave_ses=(int)msg[1];
         chave_ses=chave_ses-minhachave;
 		//comeca mensagens
 		while(strcmp(message, "tchau")){
-					printf("Você: ");
+		            cout<<"digite a mensagem"<<endl;
 					gets(message);
 					criptografa();
 					sendMessage(fdSocket,message,100*sizeof(char)); //Envia mensagem			
 					receiveMessage(fdSocket,message,100*sizeof(char)); //Recebe mensagem
 					descriptografa();
-					printf("Mensagem Recebida: %s\n", message);
+					printf("Mensagem Recebida: %s\n\n", message);
 		}
 	
 
